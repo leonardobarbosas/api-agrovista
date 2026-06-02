0494a1eee674cd4793b9fca0aafe8eb2c920e611
leonardobarbosas <leonardobarbosasantos@gmail.com>
2026-06-02 16:15:44 -0300

    inicializando api da agrovista

---
+++ src/main/java/br/com/fiap/api_agrovista/ApiAgrovistaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.api_agrovista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAgrovistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAgrovistaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/api_agrovista/ApiAgrovistaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.api_agrovista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAgrovistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
